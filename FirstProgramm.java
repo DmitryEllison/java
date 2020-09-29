97af4b89660b50a9c9b167c7449053d597c21346
DmitryEllison <71434912+DmitryEllison@users.noreply.github.com>
2020-09-30 00:03:51 +0300

    Add files via upload

---
+++ FirstProgramm.java
@@ -0,0 +1,16 @@
+public class FirstProgramm{
+	public static void main(String[] args){
+		double MyF = 9.99;
+		int MyInt = (int) MyF;
+		String greeting = " Hello, World! ";
+		System.out.println(MyInt + greeting + greeting.length());
+		if(1 >= 10){
+			System.out.println(35 == 35);
+		}else{
+			System.out.println( 12!= 12);
+		}
+		String result = (1 != 0) ? "True" : "FirstProgramm";
+		System.out.println(result);
+
+	}
+}
